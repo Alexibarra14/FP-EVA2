75e2e7c84bc65091fb7e6991064d5191ac81664b
Alexibarra14 <L24550366@chihuahua2.tecnm.mx>
2024-10-10 12:09:12 -0700

    Add files via upload

---
+++ eva2_for1/src/eva2_for1/Eva2_for1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_for1;
+
+/**
+ *
+ * @author brend
+ */
+public class Eva2_for1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++) { 
+            System.out.println(i +"-");
+        }
+        for( int i=10; i>=1;i--){ 
+            System.out.println(i +"-");
+        }
+    }
+    
+}
