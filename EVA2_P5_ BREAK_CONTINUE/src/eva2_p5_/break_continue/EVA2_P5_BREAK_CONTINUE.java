97110cd63a9cb3e5563c7a7ea6ee90de10529c03
Alexibarra14 <L24550366@chihuahua2.tecnm.mx>
2024-10-17 11:56:13 -0600

    Add files via upload

---
+++ EVA2_P5_ BREAK_CONTINUE/src/eva2_p5_/break_continue/EVA2_P5_BREAK_CONTINUE.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_p5_.break_continue;
+
+/**
+ *
+ * @author brend
+ */
+public class EVA2_P5_BREAK_CONTINUE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     for(int i = 1; i<=10;i++){
+           if(i== 8)
+               //if(i==8)continue;
+             break;
+            
+     }
+       
+            
+    }
+    
+}
