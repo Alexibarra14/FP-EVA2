ef925a8016672ceaaa3493f024f5a28d5d93e5bc
Alexibarra14 <L24550366@chihuahua2.tecnm.mx>
2024-11-04 12:52:43 -0600

    Add files via upload

---
+++ EVA2_17_arreglos3/src/eva2_17_arreglos3/EVA2_17_arreglos3.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_17_arreglos3;
+
+/**
+ *
+ * @author brend
+ */
+public class EVA2_17_arreglos3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] numero = new int[5];
+        numero[0]= 10;
+        numero[1] =20;
+        numero[2]=30;
+        numero[3]=40;
+        numero[4]=50;
+        System.out.println(" valores");
+        for (int i=0;i< numero.length;i++){
+             System.out.println(" numero pocicionado en "+i+""+numero[i]);
+             
+        }
+                
+    }
+    
+    
+}
